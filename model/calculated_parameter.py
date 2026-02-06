68b1b1a1f0085bcc4035eb28c636d71d28609dd9
novoselov_g <grisha-novoselov@mail.ru>
2026-02-06 14:45:37 +0300

    создание класса для хранения рассчитанного параметра #82

---
+++ model/calculated_parameter.py
@@ -0,0 +1,9 @@
+
+
+class Calculated_parameter():
+    
+    def __init__(self, name, value, lower_value, upper_value):
+        self.name = name
+        self.value = value
+        self.lower_value = lower_value
+        self.upper_value = upper_value
